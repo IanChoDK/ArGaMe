a0027785fb93fb4f7142d39770e1adefcea6dd63
mchena <m.chena@itecriocuarto.org.ar>
2025-10-23 21:01:19 -0300

    Biblioteca de usuario actualizada

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,24 +1,46 @@
 import { useEffect, useState } from "react"
-import { fetchGames } from "../api/api"
+import { fetchUserGames } from "../api/api"
 import GameCard from "../components/GameCard"
 import { useAuth } from "../context/AuthContext"
 
 function Library() {
     const { user } = useAuth()
     const [games, setGames] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        fetchGames().then((data) => setGames(data))
-    }, [])
+        const LoadGames = async () => {
+            try {
+            const data = await fetchUserGames(user.id)
+            setGames(data)
+            } catch (error) {
+            console.error("Error al cargar la biblioteca:", error)
+            } finally {
+            setLoading(false)
+            }
+        }
+    
+    if (user && user.id){
+        LoadGames();
+    }
+    else{
+        setLoading(false);
+    }
+}, [user])
+
 
     if (!user) return <p>Debes iniciar sesion para ver tu biblioteca.</p>
+
+    if (loading) return <p>Cargando biblioteca...</p>
+
+    if (games.length === 0) return <p>No tienes juegos en tu biblioteca.</p>
 
     return (
         <div>
         <h2>Tu Biblioteca</h2>
             <div className="game-grid">
                 {games.map((game) => (
-                <GameCard key={game.id} game={game} />
+                <GameCard key={game.game_id} game={game} />
                 ))}
             </div>
         </div>
