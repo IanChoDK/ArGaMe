9c5be7d202e25ca4bd6112bf700b52d506494035
mchena <m.chena@itecriocuarto.org.ar>
2025-10-23 21:00:52 -0300

    Actualización de GameCard

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -4,27 +4,27 @@ function GameCard({ game }) {
     return (
         <div class="card h-100 shadow-sm border-0">
             <img
-                src={game.thumbnail}
+                src={game.game.thumbnail}
                 className="card-img-top"
-                alt={game.name}
+                alt={game.game.name}
             />
             <div>
-                <h5 className="heading mb-2 text-truncate">{game.name}</h5>
+                <h5 className="heading mb-2 text-truncate">{game.game.name}</h5>
 
                 <p className="card-text small flex-grow-1">
-                {game.description}
+                {game.game.description}
                 </p>
                 
                 <div>
-                    {game.is_free ? (
+                    {game.game.is_free ? (
                     <span className="fw-bold text-primary">Gratis</span>
                     ) : (
-                    <span className="fw-bold text-primary">${game.price}</span>
+                    <span className="fw-bold text-primary">${game.game.price}</span>
                     )}
                 </div>
 
                 <div className="card-footer small">
-                    Lanzamiento: {new Date(game.release_date).toLocaleDateString("es-AR")}
+                    Lanzamiento: {new Date(game.game.release_date).toLocaleDateString("es-AR")}
                 </div>
                 
             </div>
