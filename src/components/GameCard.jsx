79e05113e86b208e85f6580c4ba3cf5c160a2168
mchena <m.chena@itecriocuarto.org.ar>
2025-10-26 21:55:37 -0300

    Link a pagina con informacion agregado

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,34 +1,35 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
 function GameCard({ game }) {
     return (
         <div class="card h-100 shadow-sm border-0">
-            <img
-                src={game.thumbnail}
-                className="card-img-top"
-                alt={game.name}
-            />
-            <div>
-                <h5 className="heading mb-2 text-truncate">{game.name}</h5>
-
-                <p className="card-text small flex-grow-1">
-                {game.description}
-                </p>
-                
+            <Link to={`/games/${game.id}`} className="card-link-wrapper">
+                <img
+                    src={game.thumbnail}
+                    className="card-img-top"
+                    alt={game.name}
+                />
                 <div>
-                    {game.is_free ? (
-                    <span className="fw-bold text-primary">Gratis</span>
-                    ) : (
-                    <span className="fw-bold text-primary">${game.price}</span>
-                    )}
-                </div>
+                    <h5 className="heading mb-2 text-truncate">{game.name}</h5>
 
-                <div className="card-footer small">
-                    Lanzamiento: {new Date(game.release_date).toLocaleDateString("es-AR")}
+                    <p className="card-text small flex-grow-1">
+                    {game.description}
+                    </p>
+
+                    <div>
+                        {game.is_free ? (
+                        <span className="fw-bold text-primary">Gratis</span>
+                        ) : (
+                        <span className="fw-bold text-primary">${game.price}</span>
+                        )}
+                    </div>
+
+                    <div className="card-footer small">
+                        Lanzamiento: {new Date(game.release_date).toLocaleDateString("es-AR")}
+                    </div>
                 </div>
-                
-            </div>
-            
+            </Link>
                 <button class="card-button">Agregar</button>
         </div>
     )
