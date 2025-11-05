4f2336a58929a79825fae3911755815c96e3c4f2
mchena <m.chena@itecriocuarto.org.ar>
2025-11-04 22:07:00 -0300

    Ruta a perfil de usuario agregada

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Profile from "./pages/Profile"
 import Library from "./pages/Library"
 import CrudGames from "./pages/UploadGame"
 import GameDetail from "./pages/GameDetail"
+import UserProfile from "./pages/UserProfile"
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
               <Route path="/register" element={<Register />} /> 
               <Route path="/store" element={<Store />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/users/:id" element={<UserProfile />} />
               <Route path="/library" element={<Library />} />
               <Route path="/UploadGame" element={<CrudGames />} />
               <Route path="/games/:id" element={<GameDetail />} />
